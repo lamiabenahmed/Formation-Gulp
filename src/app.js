f90efd260a131a7c269c24b9290c3a4e08b2f8cc
BEN HMED Lamia <lamiabenhmed@stratisworldwide.com>
2023-08-15 17:56:34 +0200

    create service section

--- src/app.js
+++ src/app.js
@@ -3,28 +3,42 @@ const loader = document.getElementById('loader-wrapper');
 const mobileMenuOverlay = document.getElementById('mobile-menu-overlay');
 const mobileMenuClose = document.getElementById('mobile-menu-close');
 const mobileMenuTrigger = document.getElementById('mobile-menu-trigger');
+const mobileMenu = document.getElementById('mobile-menu');
+
 
 window.addEventListener('load', () => {
-  if(loader) {
+  if (loader) {
     loader.remove();
   }
 });
 
-if(mobileMenuTrigger) {
+if (mobileMenuTrigger) {
   mobileMenuTrigger.addEventListener('click', (e) => {
     e.preventDefault();
-    document.body.classList.add('opened', 'lock');
+    mobileMenu.classList.toggle('is-open');
   });
 }
 
-if(mobileMenuOverlay) {
+if (mobileMenuOverlay) {
   mobileMenuOverlay.addEventListener('click', () => {
-    document.body.classList.remove('opened', 'lock');
+    mobileMenu.classList.remove('is-open');
   });
 }
 
-if(mobileMenuClose) {
+if (mobileMenuClose) {
   mobileMenuClose.addEventListener('click', () => {
-    document.body.classList.remove('opened', 'lock');
+    mobileMenu.classList.remove('is-open');
   });
 }
+
+const nunjucks = require('nunjucks');
+
+// Configurer l'environnement Nunjucks
+const env = nunjucks.configure({
+  // ... autres options de configuration ...
+});
+
+// Enregistrer la macro SvgIcon
+env.addGlobal('SvgIcon', (iconName, width, height) => {
+  return env.renderString(macro_SvgIcon, { iconName, width, height });
+});
